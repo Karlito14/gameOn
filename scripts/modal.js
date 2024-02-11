1d90f5ba8ff405c93c627a6583d54e09a09b7fd7
Karlito14 <leiroz26@hotmail.com>
2024-02-11 20:25:05 +0100

    ajout des pattern aux inputs et gestion fermeture ouverture modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,16 +1,26 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const topNav = document.getElementById("myTopnav");
+const closeButton = document.querySelector('.close');
+const modalContent = document.querySelector('.content');
+
+// Open or close the navigation
+topNav.addEventListener('click', editNav);
+function editNav(event) {
+  if (this.className === "topnav") {
+    this.className += " responsive";
+  } else {
+    this.className = "topnav";
+  }
+}
+
+modalContent.addEventListener('click', (event) => {
+  event.stopPropagation();
+})
+closeButton.addEventListener('click', closeModal);
+modalbg.addEventListener('click', closeModal)
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +28,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
