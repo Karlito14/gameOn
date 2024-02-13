6831b25f1c594f1244223ebaddbbee7acf11131b
Karlito14 <leiroz26@hotmail.com>
2024-02-13 19:15:58 +0100

    gestion du input birthdate

--- scripts/gestionInputs.js
+++ scripts/gestionInputs.js
@@ -1,8 +1,11 @@
 const inputsText = document.querySelectorAll('input[type=text]');
 const inputEmail = document.querySelector('input[type=email]');
+const inputBirthDate = document.querySelector('#birthdate');
+const inputQuantity = document.querySelector('#quantity');
 const form = document.querySelector('form[name="reserve"]');
 const insults = ['connard', 'salopard', 'abruti'];
 
+console.log(inputBirthDate.value)
 
 form.addEventListener('submit', (event) => {
     event.preventDefault(); 
@@ -10,12 +13,18 @@ form.addEventListener('submit', (event) => {
     // Boucle pour géré les 2 inputs de type text prénom et nom
     for(let input of inputsText) {
         try {
-            verifyInputsText(input, input.dataset.name);
+            verifyInputsText(input);
             displayError(input, '');
-        } 
-        catch(error) {
+        } catch(error) {
             displayError(input, error.message);
         }
+    }
+
+    try {
+        verifyInputBirthDate(inputBirthDate);
+        displayError(inputBirthDate, '');
+    } catch(error) {
+        displayError(inputBirthDate, error.message);
     }
 })
 
@@ -36,23 +45,36 @@ function displayError(input, error) {
         input.after(paragrapheError);
     } else {
         paragrapheError.remove();
-    }
-    
+    }  
 }
 
-
 // fonction qui gère la validité du nom et prénom
-function verifyInputsText(input, type) {
+function verifyInputsText(input) {
     const regexName = new RegExp("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$");
     const valueInput = input.value;
 
     if(valueInput === "") {
-        throw new Error(`Le champ ${type} ne peut etre vide`);
+        throw new Error(`Le champ ${input.dataset.name} ne peut etre vide`);
     } else if(valueInput.length < 2) {
-        throw new Error(`Le ${type} doit faire au moins 2 lettres`);
+        throw new Error(`Le ${input.dataset.name} doit faire au moins 2 lettres`);
     } else if (insults.includes(valueInput)) {
-        throw new Error(`Le ${type} ne peut contenir d'injures ! ${valueInput[0].toUpperCase() + valueInput.slice(1)} !`);
+        throw new Error(`Le ${input.dataset.name} ne peut contenir d'injures ! ${valueInput[0].toUpperCase() + valueInput.slice(1)} !`);
     } else if(!regexName.test(valueInput)) {
-        throw new Error(`Le ${type} ne peut contenir de caractères sépciales`);
+        throw new Error(`Le ${input.dataset.name} ne peut contenir de caractères sépciales`);
     }
+}
+
+function verifyInputBirthDate(input) {
+    const valueInput = input.value;
+    const arrayDate = valueInput.split('-');
+    const yearDate = +arrayDate[0];
+    if(valueInput === "") {
+        throw new Error(`Renseignez une ${input.dataset.name} de naissance`);
+    } else if(yearDate < 1950) {
+        throw new Error(`Tu ne peux pas etre si vieux !`);
+    }
+}
+
+function verifyInputEmail(input) {
+    
 }
